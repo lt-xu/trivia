9b802dcf8bade7d7790ce4841525d1ed898b2f01
SparkOfLife <ltxu1@outlook.com>
2018-09-18 10:43:58 +0800

    initial calendar

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,4 +1,30 @@
 // pages/calendar/calendar.js
+import initCalendar from '../../template/calendar/index';
+const conf = {
+  multi: false, // 是否开启多选,
+  disablePastDay: false, // 是否禁选过去的日期
+  /**
+   * 初始化日历时指定默认选中日期，如：'2018-3-6' 或 '2018-03-06'
+   * 注意：若想初始化时不默认选中当天，则将该值配置为除 undefined 以外的其他非值即可，如：空字符串, 0 ,false等。
+  */
+  defaultDay: undefined,
+  /**
+   * 选择日期后执行的事件
+   * @param { object } currentSelect 当前点击的日期
+   * @param { array } allSelectedDays 选择的所有日期（当mulit为true时，才有allSelectedDays参数）
+   */
+  // afterTapDay: (currentSelect, allSelectedDays) => { },
+  /**
+   * 日期点击事件（此事件会完全接管点击事件）
+   * @param { object } currentSelect 当前点击的日期
+   * @param { object } event 日期点击事件对象
+   */
+  // onTapDay(currentSelect, event) { },
+  /**
+   * 日历初次渲染完成后触发事件，如设置事件标记
+   */
+  // afterCalendarRender() { },
+}
 Page({
 
   /**
@@ -26,7 +52,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    initCalendar(conf); // 初始化日历
   },
 
   /**
